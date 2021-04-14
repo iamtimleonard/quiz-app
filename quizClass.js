33dd45637d5055571ecc56738529bb528728aa41
iamtimleonard <timleonardcello@gmail.com>
2021-04-14 15:53:39 -0400

    feat: track correct answer

--- quizClass.js
+++ quizClass.js
@@ -1,16 +1,20 @@
 class Quiz {
-    constructor({
-        results
-    }) {
-        this.questions = results;
-    };
-    currentQuestion = 0;
-    score = 0;
-    isActive = true;
-    getNextQuestion = () => {
-        const nextQuestion = new Question(this.questions[this.currentQuestion]);
-        this.currentQuestion++
-        return nextQuestion
-    };
-    handleResult = (result) => result ? this.score++ : null;
+  constructor({ results }) {
+    this.questions = results;
+  }
+  currentQuestion = 0;
+  score = 0;
+  isActive = true;
+  getNextQuestion = () => {
+    const nextQuestion = new Question(this.questions[this.currentQuestion]);
+    this.currentQuestion++;
+    return nextQuestion;
+  };
+
+  handleResult = (result) => {
+    if (result) {
+      this.score++;
+    }
+    this.questions[this.currentQuestion - 1].correct = result;
+  };
 }
